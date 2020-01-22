b4437e2f2798f8ab6342dc59d16a3355a697dbca
felix <felix@LAPTOP-1PNMTCQM>
2020-01-22 16:12:17 +0100

    Added scrollbar

--- src/client/window/panels/menus/MenuGrid.java
+++ src/client/window/panels/menus/MenuGrid.java
@@ -3,71 +3,183 @@ package client.window.panels.menus;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.util.ArrayList;
+
+import javax.swing.JPanel;
 
 public class MenuGrid extends Menu {
 	private static final long serialVersionUID = 1941339088614372748L;
 
-	/** Set rowHeight to SQUARE will adjust the height to equals the width */
-	public static final int SQUARE = -1;
-
-	/** Set padding to GRID_SPACE will adjust the padding to match the gridSpace */
-	public static final int GRID_SPACE = -1;
-
+	// ======================= Grid size =========================
 	/** Number of columns in the grid */
 	private int cols = 4;
 
 	/** Height of the rows in the grid */
 	private int rowHeight = SQUARE;
-
-	/** Number of pixels between the items of the grid */
-	private int gridSpace = 2;
-
-	/** Number of pixel between the border and the grid */
-	private int padding = 0;
-
-	private Color backgroundColor = Color.LIGHT_GRAY;
-	private Color borderColor = Color.DARK_GRAY;
+	/** Set rowHeight to SQUARE will adjust the height to equals the width */
+	public static final int SQUARE = -1;
 
 	private int borderSize = 0;
 
+	/** Number of pixels between the items of the grid */
+	private int gridSpace = 2;
+	/** Number of pixel between the border and the grid */
+	private int padding = 0;
+	/** Set padding to GRID_SPACE will adjust the padding to match the gridSpace */
+	public static final int GRID_SPACE = -1;
+
+	// ======================= Color =========================
+	private Color backgroundColor = Color.LIGHT_GRAY;
+	private Color borderColor = Color.DARK_GRAY;
+
+	// ======================= Scroll =========================
+	private JPanel scrollBar;
+	private int scrollWidth = 10;
+	private int scrollClick = -1;
+	/** Number of pixels to decal (on scroll) */
+	private int scrolled = 0;
+	/** Number of pixels to decal on one scroll-tick */
+	private int scrollStep = 20;
+	/** MAximum number of pixels that can be scrolled */
+	private int visibleHeight = 0;
+
+	// ======================= Menus =========================
 	/** List of the panels in the grid */
-	ArrayList<Menu> list = new ArrayList<>();
+	private ArrayList<Menu> list = new ArrayList<>();
+
+	// =========================================================================================================================
+
+	public MenuGrid() {
+		scrollBar = new JPanel();
+		scrollBar.setBackground(Color.DARK_GRAY);
+		scrollBar.setVisible(false);
+		add(scrollBar);
+
+		scrollBar.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				scrollClick = e.getYOnScreen();
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+
+		scrollBar.addMouseMotionListener(new MouseMotionListener() {
+			@Override
+			public void mouseMoved(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				scrolled += (int) ((e.getYOnScreen() - scrollClick) * ((double) getHeight() / visibleHeight));
+				scrollClick = e.getYOnScreen();
+
+				updateScroll();
+				updateMenu();
+			}
+		});
+
+		// ======================= Wheel =========================
+
+		addMouseWheelListener(new MouseWheelListener() {
+			@Override
+			public void mouseWheelMoved(MouseWheelEvent e) {
+				if (visibleHeight < getHeight()) {
+					e.consume();
+
+					scrolled += e.getWheelRotation() * scrollStep;
+
+					updateScroll();
+					updateMenu();
+				}
+			}
+		});
+	}
 
 	// =========================================================================================================================
 
 	@Override
 	protected void paintComponent(Graphics g) {
-		g.setColor(borderColor);
-		g.fillRect(0, 0, getWidth() - 1, getHeight() - 1);
-
 		g.setColor(backgroundColor);
-		g.fillRect(borderSize, borderSize, getWidth() - 1 - borderSize * 2, getHeight() - 1 - borderSize * 2);
+		g.fillRect(borderSize, borderSize, getWidth() - borderSize * 2, getHeight() - borderSize * 2);
+
+		g.setColor(borderColor);
+		for (int i = 0; i < borderSize; i++)
+			g.drawRect(i, i, getWidth() - 1 - 2 * i, getHeight() - 1 - 2 * i);
 	}
 
 	// =========================================================================================================================
 
 	public void addMenu(Menu menu) {
-		int row = list.size() / cols;
-		int col = list.size() % cols;
-
-		setMenu(menu, row, col);
+		setMenu(menu, list.size() / cols, list.size() % cols);
 
 		list.add(menu);
-		add(menu);
+		add(menu, -1);
+
+		updateSize();
+		updateScroll();
 	}
 
-	public void setMenu(Menu menu, int row, int col) {
+	private void setMenu(Menu menu, int row, int col) {
 		int padding = this.padding == GRID_SPACE ? gridSpace : this.padding;
 
 		int w = (getWidth() - (cols - 1) * gridSpace - 2 * (borderSize + padding)) / cols;
 		int wMore = (getWidth() - (cols - 1) * gridSpace - 2 * (borderSize + padding)) % cols;
 		int h = rowHeight == SQUARE ? w : rowHeight;
 
-		menu.setLocation(borderSize + padding + (w + gridSpace) * col, borderSize + padding + (h + gridSpace) * row);
-		menu.setSize(w + (col == cols - 1 ? wMore : 0), h);// w + (col == cols - 1 ? getWidth() - cols * w : 0)
+		menu.setLocation(borderSize + padding + (w + gridSpace) * col,
+				-scrolled + borderSize + padding + (h + gridSpace) * row);
+		menu.setSize(w + (col == cols - 1 ? wMore : 0), h);
+	}
 
-		super.setSize(getWidth(), (row + 1) * h + row * gridSpace + 2 * (borderSize + padding));
+	private void updateMenu() {
+		for (int i = 0; i < list.size(); i++)
+			setMenu(list.get(i), i / cols, i % cols);
+	}
+
+	private void updateSize() {
+		int padding = this.padding == GRID_SPACE ? gridSpace : this.padding;
+
+		int menuWidth = (getWidth() - (cols - 1) * gridSpace) / cols;
+		int rows = list.size() / cols + (list.size() % cols == 0 ? 0 : 1);
+
+		super.setSize(getWidth(), 2 * (borderSize + padding) + rows * (rowHeight == SQUARE ? menuWidth : rowHeight)
+				+ (rows - 1) * gridSpace);
+	}
+
+	// =========================================================================================================================
+
+	public void updateScroll() {
+		scrollBar.setVisible(visibleHeight < getHeight());
+
+		if (visibleHeight >= getHeight())
+			return;
+
+		scrolled = Math.max(0, scrolled);
+		scrolled = Math.min(getHeight() - visibleHeight, scrolled);
+
+		double ratio = visibleHeight / (double) getHeight();
+
+		scrollBar.setLocation(getWidth() - scrollWidth, (int) (scrolled * ratio));
+		scrollBar.setSize(scrollWidth, (int) (visibleHeight * ratio));
 	}
 
 	// =========================================================================================================================
@@ -82,11 +194,6 @@ public class MenuGrid extends Menu {
 
 	public void clear() {
 		list.clear();
-
-		int size = (getWidth() - (cols - 1) * gridSpace) / cols;
-		int rows = list.size() / cols + (list.size() % cols == 0 ? 0 : 1);
-
-		super.setSize(getWidth(), rows * (rowHeight == SQUARE ? size : rowHeight) + (rows - 1) * gridSpace);
 	}
 
 	// =========================================================================================================================
@@ -112,26 +219,13 @@ public class MenuGrid extends Menu {
 
 	@Override
 	public void setSize(int width, int height) {
-		int size = (width - (cols - 1) * gridSpace) / cols;
+		super.setSize(width, height);
 
-		for (int i = 0; i < list.size(); i++) {
-			Menu menu = list.get(i);
-			int row = i / cols;
-			int col = i % cols;
+		visibleHeight = height;
 
-			setMenu(menu, row, col);
-
-			// menu.setLocation((size + gridSpace) * col, ((rowHeight == SQUARE ? size :
-			// rowHeight) + gridSpace) * row);
-			// menu.setSize(size + (col == cols - 1 ? getWidth() - cols * size : 0),
-			// rowHeight == SQUARE ? size : rowHeight);
-		}
-
-		int rows = list.size() / cols + (list.size() % cols == 0 ? 0 : 1);
-		if (rows == 0)
-			rows = 1;
-
-		super.setSize(width, rows * (rowHeight == SQUARE ? size : rowHeight) + (rows - 1) * gridSpace);
+		updateSize();
+		updateScroll();
+		updateMenu();
 	}
 
 	@Override
